3974e9c427515d00264e5ec1903544deae8d9dda
Sumon <sumon@kaz-software.com>
2026-02-15 21:05:14 +0600

    project file path corrected

--- Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net9.0/Task2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6b1f1ff048b9d82a2b2c56eedab443e02c78586")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
